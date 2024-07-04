319f82218b9d56f8b9e25317e390de917a0028be
EkaterinaBatullina <153987864+EkaterinaBatullina@users.noreply.github.com>
2024-07-04 19:20:49 +0300

    Update data.sql

--- Task02/sql/data.sql
+++ Task02/sql/data.sql
@@ -1,5 +1,23 @@
-insert into student (first_name, last_name, age, email)
-values('Marsel','Sidikov',30,'111'),
-      ('Maxim','Pozdeev',28,'222');
+insert into client (first_name, last_name, age)
+values('Marat', 'Hafizov', 30),
+      ('Maria','Nikolaeva', 25),
+      ('Ayrat', 'Sidikov', 41);
 
-update student set age = 31 where id = 1;
+update client set age = 31 where id = 2;
+
+insert into driver (first_name, last_name, experience)
+values('Marsel', 'Zaripov', 7),
+      ('Anna','Safina', 12),
+      ('Aleksey', 'Galimov', 10);
+
+update driver set experience = 8 where id = 2;
+
+insert into auto (name, number, driver_id)
+values('Kia', 'к724уу', 1),
+      ('Ford', 'о814ое', 2),
+      ('Toyota', 'е432ех', 3);
+
+insert into "order" (start_time, client_id, driver_id)
+values('09:45:17', 3, 1),
+      ('12:30:00', 2, 2),
+      ('20:11:56', 1, 3);
